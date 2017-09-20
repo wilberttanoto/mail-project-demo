9ea048eb9b43e054be1407b99c7266b26a85812b
Wilbert Tanoto <wilbert.ext@gmail.com>
2017-09-20 10:58:07 +0700

    Datetime fields flask-wtf as timestamp in the email view

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField, DateTimeField, TextAreaField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 class MailForm(FlaskForm):
 	"""
@@ -9,3 +10,7 @@ class MailForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired()])
 	email_body = TextAreaField('Email Body', validators=[DataRequired()])
 	email_subject = StringField('Email Subject', validators=[DataRequired()])
+	timestamp = DateTimeField("Time Stamp",
+                          format="%Y-%m-%d %H:%M:%S", 
+                          default=datetime.today,
+                          validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ from .forms import MailForm
 
 from models import MailMail
 
+from datetime import datetime
+
 @app.route('/')
 @app.route('/save_emails', methods=['GET', 'POST'])
 def list_emails():
@@ -25,17 +27,20 @@ def add_email():
 	form = MailForm()
 	add_email = True
 
-	if form.validate_on_submit():
+	if request.method == 'POST' and form.validate_on_submit():
 		email = form.email.data
 		email_subject = form.email_subject.data
 		email_body = form.email_body.data
+		# timestamp = datetime.strptime(form.timestamp.data,'%Y-%m-%d %H:%M:%S')
+		timestamp = form.timestamp.data
 
-		email = MailMail(email=email,email_subject=email_subject,email_body=email_body)
+		email = MailMail(email=email,email_subject=email_subject,email_body=email_body,timestamp=timestamp)
 		try:
 			db.session.add(email)
 			db.session.commit()
 			flash('You have successfully added a new email.')
 		except:
+			raise
 			flash('Something went wrong with the email.')
 		return redirect(url_for('list_emails'))
 	else:
@@ -50,10 +55,11 @@ def edit_email(id):
 	email = MailMail.query.get_or_404(id)
 	form = MailForm(obj=email)
 	
-	if form.validate_on_submit():
+	if request.method == 'POST' and form.validate_on_submit():
 		email.email = form.email.data
 		email.email_subject = form.email_subject.data
 		email.email_body = form.email_body.data
+		email.timestamp = form.timestamp.data
 		try:
 			db.session.add(email)
 			db.session.commit()
@@ -65,6 +71,7 @@ def edit_email(id):
 		form.email.data = email.email
 		form.email_subject.data = email.email_subject
 		form.email_body.data = email.email_body
+		form.timestamp.data = email.timestamp
 		return render_template("edit_email.html", form=form)
 
 @app.route('/save_emails/delete/<int:id>', methods=['GET', 'POST'])
