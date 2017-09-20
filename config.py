2ae92b95116afaf1ae5319285defaf9c78088d2e
Wilbert Tanoto <wilbert.ext@gmail.com>
2017-09-20 08:52:50 +0700

    Moving configuration from run.py to config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SECRET_KEY = 'super secret key'
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
